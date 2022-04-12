fc61946453f768eb5daf4cce1707aed704a0b32a
mesu-ai <mesu.s.ss@gmail.com>
2022-04-12 12:06:12 +0600

    user response table

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+document.getElementById('alertShowId').style.display='none';
+document.getElementById('alertFailedId').style.display='none';
+document.getElementById('loadingId').style.display='none';
+
+const handleAction=()=>{
+    document.getElementById('loadingId').style.display='block';
+
+    const userId= document.getElementById('userId').innerText;
+    const userName= document.getElementById('userName').innerText;
+    
+    const data={'UserId':userId,'UserName':userName};
+    // console.log(data);
+
+
+    // data send to server
+    fetch('http://127.0.0.1:8000/changing-state',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+
+        document.getElementById('alertShowId').style.display='block';
+        document.getElementById('loadingId').style.display='none';
+        console.log('Success:', result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('loadingId').style.display='none';
+        document.getElementById('alertFailedId').style.display='block';
+
+        // return <p>error</p>
+
+    });
+
+}
+
+const handleAlert=()=>{
+    document.getElementById('alertShowId').style.display='none'
+    document.getElementById('alertFailedId').style.display='none';
+
+    // console.log('clicked');
+
+}
