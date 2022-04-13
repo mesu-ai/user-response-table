236dfdcac0c31617869f910e94cc89e9d617fdbb
mesu-ai <mesu.s.ss@gmail.com>
2022-04-13 12:07:58 +0600

    update dynamicity

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
 
-
 document.getElementById('alertShowId').style.display='none';
 document.getElementById('alertFailedId').style.display='none';
 document.getElementById('loadingId').style.display='none';
 
-const handleAction=()=>{
-    document.getElementById('loadingId').style.display='block';
 
-    const userId= document.getElementById('userId').innerText;
-    const userName= document.getElementById('userName').innerText;
+const dataLoad=()=>{
+    fetch('./data.json')
+    .then(res=>res.json())
+    .then(data=>{
+        // console.log(data);
+         showData(data);
+    
+    })
+
+}
+
+dataLoad();
+
+
+const showData=(data)=>{
+
+    for (const [idx,cellData] of data.entries()) {
+       // console.log(cellData,idx);
+
+
+       const tableBody=document.getElementById('tbodyID');
+       const tr=document.createElement('tr');
+       tr.innerHTML=`
+       
+          <th scope="row">${idx+1}</th>
+          <td >${cellData.userId}</td>
+          <td >${cellData.userName}</td>
+        <td>
+            <label onclick="handleAction('${cellData?.userId}','${cellData?.userName}')" class="switch">
+            <input type="checkbox">
+            <span class="slider round"></span>
+            </label>
+        </td>
+       `
+       tableBody.appendChild(tr);
+        
+    }
+
+
+}
+
+const handleButton=(id)=>{
+    console.log(id);
+
+}
+
+
+const handleAction=(userId,userName)=>{
+   
+    document.getElementById('loadingId').style.display='block';
     
     const data={'UserId':userId,'UserName':userName};
-    // console.log(data);
+     console.log(data);
 
 
     // data send to server
-    const url='http://127.0.0.1:8000/changing-state';
+    const url='http://localhost:5000/users';
     fetch(url,{
         method:'POST',
         headers:{
